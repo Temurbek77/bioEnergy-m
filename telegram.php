0e3e41f0d2c8c0bc751b770aad3b434cbfe72198
Temurbek <temurweyx777@gmail.com>
2022-11-13 13:58:39 +0300

    rename....

---
+++ telegram.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 0);
+
+ini_set('display_errors','0');
+error_reporting(E_ALL);
+/* https://api.telegram.org/bot5695258170:AAHdfcF7Uw7Zdob9i5EMdVuq4L4EWDgO3Dw/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$address = $_POST['user_address'];
+$product = $_POST['product'];
+$commit = $_POST['user_commit'];
+$token = "5695258170:AAHdfcF7Uw7Zdob9i5EMdVuq4L4EWDgO3Dw";
+$chat_id = "-751084944";
+$arr = array(
+    'Buyurtmachi: ' => $name,
+    'telefon-raqami: ' => $phone,
+    'manzili: ' => $address,
+    'buyurtma soni: ' => $product,
+    'komentariya: ' => $commit
+);
+
+$txt = $txt;
+foreach ($arr as $key => $value) {
+    $txt .= "<b>" . $key . "</b> " . $value . "%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
+
+if ($sendToTelegram) {
+    header('Location: thanks.html');
+} else {
+    echo "Error";
+}
+
+?>
+
